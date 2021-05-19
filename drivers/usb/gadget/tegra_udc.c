d9de009ce615a2eaff53e166ee017f6a2756d60b
killallpie <killallpie@inbox.ru>
2021-05-19 03:31:21 -0500

    asd

--- drivers/usb/gadget/tegra_udc.c
+++ drivers/usb/gadget/tegra_udc.c
@@ -52,8 +52,8 @@
 
 #include "../../../arch/arm/mach-tegra/usb_phy.c"
 #include "../otg/ulpi_viewport.c"
-#include "../../../arch/arm/mach-tegra/fuse.h"
-#include "../../../arch/arm/mach-tegra/pinmux.c"
+#include <mach/fuse.h>
+#include <mach/pinmux.h>
 
 //#define IS_NEW_PHY_DRIVER 1
 
